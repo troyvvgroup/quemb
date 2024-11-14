74e0bb4a1342bcbfabf09f6dcb0bc27cccf62653
Oskar Weser <oskar.weser@gmail.com>
2024-11-14 14:08:01 -0500

    fixed fcidump writer test
    set OMP_NUM_THREAD=1 makes the MO-coefficients deterministic
    https://github.com/pyscf/pyscf/issues/2243
    
    thanks Minsik :-)

--- tests/molbe_io_fcidump_test.py
+++ tests/molbe_io_fcidump_test.py
@@ -1,13 +1,12 @@
 # Illustrates how fcidump file containing fragment hamiltonian
 # can be generated using be2fcidump
 
-import os
 from pathlib import Path
 from shutil import rmtree
 from tempfile import mkdtemp
 
 import numpy as np
-import pytest
+from pyscf.lib.misc import with_omp_threads
 from pyscf.tools import fcidump
 
 from molbe import BE, fragpart
@@ -20,7 +19,11 @@ def prepare_system():
     mol, mf = libint2pyscf(
         "data/octane.xyz", "data/hcore_libint_octane.dat", "STO-3G", hcore_skiprows=1
     )
-    mf.kernel()
+    with with_omp_threads(1):
+        # multi-threaded HF execution can lead to non-deterministic
+        # MO-coefficients, if the orbitals are degenerate.
+        # https://github.com/pyscf/pyscf/issues/2243
+        mf.kernel()
 
     # Construct fragments for BE
     fobj = fragpart(be_type="be2", mol=mol)
@@ -41,21 +44,14 @@ def verify_fcidump_writing(kind_of_MO : str):
         reference = fcidump.read(data_dir / kind_of_MO / f'octanef{i}')
         new = fcidump.read(tmp_dir / kind_of_MO / f'octanef{i}')
 
-        if (abs(new["H1"] - reference["H1"]).max() > 1e-5):
-            print(reference["H1"][abs(new["H1"] - reference["H1"]) > 1e-5])
         assert np.allclose(new["H1"], reference["H1"])
         assert np.allclose(new["H2"], reference["H2"])
     rmtree(tmp_dir)
 
 
-@pytest.mark.skipif(not os.getenv("QUEMB_DO_KNOWN_TO_FAIL_TESTS") == "true",
-                    reason="This test is known to fail.")
 def test_embedding():
     verify_fcidump_writing("embedding")
 
 
-@pytest.mark.skipif(not os.getenv("QUEMB_DO_KNOWN_TO_FAIL_TESTS") == "true",
-                    reason="This test is known to fail.")
 def test_fragment_mo():
     verify_fcidump_writing("fragment_mo")
-
