5c9695ffa4138b9d31a336861ea9ec45a148278d
Oskar Weser <oskar.weser@gmail.com>
2024-12-10 17:23:46 -0500

    fixed mypy of scratch manager

--- src/quemb/shared/manage_scratch.py
+++ src/quemb/shared/manage_scratch.py
@@ -17,6 +17,10 @@ def _to_abs_path(pathlike: PathLike) -> Path:
     return Path(pathlike).resolve()
 
 
+def _get_absolute_path(pathlike: PathLike) -> Path:
+    return Path(pathlike).resolve()
+
+
 @define(order=False)
 class WorkDir:
     """Manage a scratch area.
