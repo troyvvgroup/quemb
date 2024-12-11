9f1e9e8fc3d692c2fcb159e78e1d7f65d748a77a
Oskar Weser <oskar.weser@gmail.com>
2024-12-11 15:56:49 -0500

    cleaned up autofrag
    removed unused arguments and
    cleaned redundant if-else branches

--- src/quemb/kbe/autofrag.py
+++ src/quemb/kbe/autofrag.py
@@ -190,10 +190,6 @@ def kfrag_func(
     uNs,
     Ns,
     nk2=None,
-    debug=False,
-    debug1=False,
-    shift=False,
-    debug2=False,
 ):
     if nk2 is None:
         nk2 = nk1
@@ -207,13 +203,8 @@ def kfrag_func(
             else:
                 frglist.append(uNs * numk + pq)
         elif numk == nk1:
-            if not uNs == Ns:
-                frglist.append(uNs * numk + pq)
-            else:
-                if not debug2:
-                    frglist.append(uNs * numk + pq)
-                else:
-                    frglist.append(uNs * numk + pq)
+            frglist.append(uNs * numk + pq)
+
         elif numk > nk1:
             if uNs == Ns:
                 frglist.append(uNs * numk + pq)
@@ -221,15 +212,7 @@ def kfrag_func(
                 nk_ = numk - nk1
                 frglist.append(uNs * nk1 + (nk_ * uNs) + pq)
         else:
-            if not Ns == uNs:
-                frglist.append(uNs * numk + pq)
-
-            else:
-                frglist.append(uNs * numk + pq)
-                if debug:
-                    print(uNs * numk + pq, end=" ")
-    if debug:
-        print()
+            frglist.append(uNs * numk + pq)
     return frglist
 
 
