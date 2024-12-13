8f94610b598c35c76fc5f31eeb8c00402b488b1a
Shaun Weatherly <wshaunryan@gmail.com>
2024-12-13 15:12:32 -0500

    Added default dmrg `settings.py` to `external`, modified workflow, + initial testing.

---
+++ src/quemb/shared/external/dmrgscf_settings.py
@@ -0,0 +1,18 @@
+#
+#This file sets the environment.
+#Configs will be used in dmrgsci.py and chemps2.py
+#
+
+import os
+from pyscf import lib
+
+#For Block and Block2 solvers
+BLOCKEXE = 'block2main'
+BLOCKEXE_COMPRESS_NEVPT = 'block2main'
+BLOCKSCRATCHDIR = str(os.getpid())
+BLOCKRUNTIMEDIR = str(os.getpid())
+MPIPREFIX = ''
+BLOCKVERSION = None
+
+#For chemps2 solvers
+#PYCHEMPS2BIN = '/path/to/CheMPS2/build/PyCheMPS2/PyCheMPS2.so'
--- tests/dmrg_molBE_test.py
+++ tests/dmrg_molBE_test.py
@@ -19,8 +19,8 @@ except ImportError:
 
 class TestBE_DMRG(unittest.TestCase):
     @unittest.skipIf(
-        not os.getenv("QUEMB_DO_KNOWN_TO_FAIL_TESTS") == "true",
-        reason="This test is known to fail.",
+        dmrgscf is None,
+        reason="Optional module 'dmrgscf' not imported correctly.",
     )
     def test_h8_sto3g_pipek(self):
         mol = gto.M()
