97ad80bb9f69837485c975705e9b9bd1d4c62842
Shaun Weatherly <wshaunryan@gmail.com>
2025-01-22 16:59:46 -0500

    Edit `centerf_idx`

--- src/quemb/molbe/autofrag.py
+++ src/quemb/molbe/autofrag.py
@@ -320,7 +320,7 @@ def graphgen(
     # Update relative center site indices (centerf_idx) and weights
     # for center site contributions to the energy (ebe_weights):
     for adx, center in enumerate(fragment_map.center):
-        centerf_idx = tuple([fragment_map.fsites[adx].index(cdx) for cdx in center])
+        centerf_idx = tuple(fragment_map.fsites[adx].index(cdx) for cdx in center)
         ebe_weight = (1.0, tuple(centerf_idx))
         fragment_map.centerf_idx.append(centerf_idx)
         fragment_map.ebe_weights.append(ebe_weight)
