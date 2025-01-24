2e952f6ce064cd431f37633904a446bd043f3412
Oskar Weser <oskar.weser@gmail.com>
2025-01-23 19:04:05 -0500

    finished FragmentedMolecule :D

--- src/quemb/molbe/chemfrag.py
+++ src/quemb/molbe/chemfrag.py
@@ -1,33 +1,26 @@
 from collections import defaultdict
+from collections.abc import Sequence
 from typing import Final, NewType, Self, TypeAlias, cast
 
 from attr import define
 from chemcoord import Cartesian
 from ordered_set import OrderedSet
+from pyscf.gto import Mole
 
 from quemb.shared.typing import T
-
-#: The index of an atom.
-AtomIdx = NewType("AtomIdx", int)
 
 #: The index of an atomic orbital.
 AOIdx = NewType("AOIdx", int)
 
-#: The index of a site in the molecule.
-#: By site we mean the heavy atom including its H- coordination sphere,
-#: i.e. CH2 is a site, CH3 is a site, etc.
-#: In the case where we do not treat hydrogens differently,
-#: the site index is the same as the atom index.
-SiteIdx = NewType("SiteIdx", AtomIdx)
 
-#: The index of a center of a fragment.
-CenterIdx = NewType("CenterIdx", SiteIdx)
+#: The index of an atom.
+AtomIdx = NewType("AtomIdx", int)
 
-# #: A dictionary that maps a Site index, the center,
-# #: to a set of atom indices, the corresponding fragment.
-# AtomPerFrag = NewType("AtomPerFrag", dict[SiteIdx, OrderedSet[AtomIdx]])
+CenterIdx = NewType("CenterIdx", AtomIdx)
+EdgeIdx = NewType("EdgeIdx", AtomIdx)
+MotifIdx: TypeAlias = CenterIdx | EdgeIdx
 
-SitePerFrag: TypeAlias = dict[CenterIdx, OrderedSet[SiteIdx]]
+OriginIdx = NewType("OriginIdx", CenterIdx)
 
 
 #: A dictionary that maps an atom index, the center,
@@ -50,23 +43,18 @@ SitePerFrag: TypeAlias = dict[CenterIdx, OrderedSet[SiteIdx]]
 #:      if (j_center in contained_center_indices[i_center])
 #            and fragments[j_center] == fragments[i_center]):
 #:          i_center <= j_center
-ContainedCenterIdx = NewType("ContainedCenterIdx", dict[CenterIdx, OrderedSet[SiteIdx]])
-
-
-def merge_sets(*sets: OrderedSet[T]) -> OrderedSet[T]:
-    # mypy wrongly complains that the arg type is not valid, which it is.
-    return OrderedSet().union(*sets)  # type: ignore[arg-type]
-
-
-@define
-class FragmentedMolecule:
-    site_per_frag: Final[SitePerFrag]
-    contained_center_indices: Final[ContainedCenterIdx]
-
+ContainedCenterIdx = NewType(
+    "ContainedCenterIdx", dict[OriginIdx, OrderedSet[CenterIdx]]
+)
 
 #: A dictionary that maps an atom index, the center,
 #: to a set of AO indices in the corresponding BE fragment.
 AOPerFrag = NewType("AOPerFrag", dict[CenterIdx, OrderedSet[AOIdx]])
+
+
+def merge_sets(*sets: Sequence[T]) -> OrderedSet[T]:
+    # mypy wrongly complains that the arg type is not valid, which it is.
+    return OrderedSet().union(*sets)  # type: ignore[arg-type]
 
 
 @define
@@ -75,14 +63,16 @@ class ConnectivityData:
 
     #: The connectivity graph of the molecule.
     bonds: Final[dict[AtomIdx, OrderedSet[AtomIdx]]]
-    #: The site/heavy atoms in the molecule.
-    sites: Final[OrderedSet[SiteIdx]]
-    #: The site connectivity graph of the molecule, i.e. ignoring the hydrogen atoms.
-    site_bonds: Final[dict[SiteIdx, OrderedSet[SiteIdx]]]
+    #: The heavy atoms/motifs in the molecule.
+    heavy_atoms: Final[OrderedSet[MotifIdx]]
+    #: The connectivity graph of the heavy atoms, i.e. ignoring the hydrogen atoms.
+    heavy_atom_bonds: Final[dict[MotifIdx, OrderedSet[MotifIdx]]]
     #: The hydrogen atoms in the molecule.
     H_atoms: Final[OrderedSet[AtomIdx]]
-    #: The hydrogen atoms per site.
-    H_per_site: Final[dict[SiteIdx, OrderedSet[AtomIdx]]]
+    #: The hydrogen atoms per motif.
+    H_per_motif: Final[dict[MotifIdx, OrderedSet[AtomIdx]]]
+    #: All atoms per motif. Lists the heavy atom first.
+    atoms_per_motif: Final[dict[MotifIdx, OrderedSet[AtomIdx]]]
 
     @classmethod
     def from_cartesian(cls, m: Cartesian) -> Self:
@@ -90,13 +80,19 @@ class ConnectivityData:
             raise ValueError("We assume 0-indexed data for the rest of the code.")
         m = m.sort_index()
         bonds = {k: OrderedSet(sorted(v)) for k, v in m.get_bonds().items()}
-        sites = OrderedSet(m.loc[m.atom != "H", :].index)
-        site_bonds = {site: bonds[site] & sites for site in sites}
-        H_atoms = OrderedSet(m.index).difference(sites)
-        H_per_site = {i_site: bonds[i_site] & H_atoms for i_site in sites}
-        return cls(bonds, sites, site_bonds, H_atoms, H_per_site)
+        heavy_atoms = OrderedSet(m.loc[m.atom != "H", :].index)
+        site_bonds = {site: bonds[site] & heavy_atoms for site in heavy_atoms}
+        H_atoms = OrderedSet(m.index).difference(heavy_atoms)
+        H_per_motif = {i_site: bonds[i_site] & H_atoms for i_site in heavy_atoms}
+        atoms_per_motif = {
+            i_site: merge_sets([i_site], H_atoms)
+            for i_site, H_atoms in H_per_motif.items()
+        }
+        return cls(
+            bonds, heavy_atoms, site_bonds, H_atoms, H_per_motif, atoms_per_motif
+        )
 
-    def get_BE_fragment(self, i_center: SiteIdx, n_BE: int) -> OrderedSet[SiteIdx]:
+    def get_BE_fragment(self, i_center: MotifIdx, n_BE: int) -> OrderedSet[MotifIdx]:
         """Return the BE fragment around atom :code:`i_center`.
 
         Return the index of the site atoms of the fragment that
@@ -108,22 +104,32 @@ class ConnectivityData:
         result = OrderedSet({i_center})
         new = result.copy()
         for _ in range(n_BE - 1):
-            new = merge_sets(*(self.site_bonds[i] for i in new)).difference(result)
+            new = merge_sets(*(self.heavy_atom_bonds[i] for i in new)).difference(
+                result
+            )
             if not new:
                 break
             result = result.union(new)
         return result
 
-    def all_fragments_sites_only(self, n_BE: int) -> dict[SiteIdx, OrderedSet[SiteIdx]]:
-        return {i: self.get_BE_fragment(i, n_BE) for i in self.sites}
+    def all_fragments_sites_only(
+        self, n_BE: int
+    ) -> dict[MotifIdx, OrderedSet[MotifIdx]]:
+        return {i: self.get_BE_fragment(i, n_BE) for i in self.heavy_atoms}
+
+
+@define
+class SubsetsCleaned:
+    motif_per_frag: Final[dict[OriginIdx, OrderedSet[MotifIdx]]]
+    contained_center_indices: Final[ContainedCenterIdx]
 
 
 def cleanup_if_subset(
-    fragment_indices: dict[SiteIdx, OrderedSet[SiteIdx]],
-) -> FragmentedMolecule:
+    fragment_indices: dict[MotifIdx, OrderedSet[MotifIdx]],
+) -> SubsetsCleaned:
     """Remove fragments that are subsets of other fragments."""
     contain_others = ContainedCenterIdx(defaultdict(OrderedSet))
-    subset_of_others: set[SiteIdx] = set()
+    subset_of_others: set[CenterIdx] = set()
 
     for i_center, i_fragment in fragment_indices.items():
         if i_center in subset_of_others:
@@ -131,22 +137,62 @@ def cleanup_if_subset(
         for j_center in i_fragment:
             if i_center == j_center:
                 continue
+            j_center = cast(CenterIdx, j_center)
             if fragment_indices[j_center].issubset(i_fragment):
-                i_center = cast(CenterIdx, i_center)
+                i_center = cast(OriginIdx, i_center)
                 subset_of_others.add(j_center)
                 contain_others[i_center] |= {j_center}
                 if j_center in contain_others:
-                    j_center = cast(CenterIdx, j_center)
+                    j_center = cast(OriginIdx, j_center)
                     contain_others[i_center] |= contain_others[j_center]
                     del contain_others[j_center]
-    return FragmentedMolecule(
+    return SubsetsCleaned(
         {
-            CenterIdx(k): v
+            OriginIdx(CenterIdx(k)): v
             for k, v in fragment_indices.items()
             if k not in subset_of_others
         },
         contain_others,
     )
+
+
+@define
+class FragmentedMolecule:
+    atoms_per_frag: Final[dict[OriginIdx, OrderedSet[AtomIdx]]]
+    motif_per_frag: Final[dict[OriginIdx, OrderedSet[MotifIdx]]]
+    contained_center_indices: Final[ContainedCenterIdx]
+    conn_data: Final[ConnectivityData]
+    n_BE: Final[int]
+
+    @classmethod
+    def from_motifs(cls, conn_data: ConnectivityData, n_BE: int) -> Self:
+        fragments = cleanup_if_subset(
+            {
+                i_center: conn_data.get_BE_fragment(i_center, n_BE)
+                for i_center in conn_data.heavy_atoms
+            }
+        )
+        atoms_per_frag = {
+            i_origin: merge_sets(
+                *[conn_data.atoms_per_motif[i_motif] for i_motif in i_fragment]
+            )
+            for i_origin, i_fragment in fragments.motif_per_frag.items()
+        }
+        return cls(
+            atoms_per_frag,
+            fragments.motif_per_frag,
+            fragments.contained_center_indices,
+            conn_data,
+            n_BE,
+        )
+
+    @classmethod
+    def from_cartesian(cls, mol: Cartesian, n_BE: int) -> Self:
+        return cls.from_motifs(ConnectivityData.from_cartesian(mol), n_BE)
+
+    @classmethod
+    def from_Mol(cls, mol: Mole, n_BE: int) -> Self:
+        return cls.from_cartesian(mol.to_pysf(), n_BE)
 
 
 # def add_back_H(m: Cartesian, n_BE: int, fragments: AtomPerFrag) -> AtomPerFrag:
