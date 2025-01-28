e8503f0de3f44b751b69ebf84f4d493c776e4e98
Oskar Weser <oskar.weser@gmail.com>
2025-01-27 22:55:51 -0500

    check for not isclose in failing test

--- tests/suspected_bug_in_fragmentation.py
+++ tests/suspected_bug_in_fragmentation.py
@@ -9,7 +9,7 @@ from quemb.molbe.mbe import BE
 def test_matching_order():
     def get_energy(path):
         m = Cartesian.read_xyz(path)
-        mol = m.to_pyscf()
+        mol = m.to_pyscf(basis="sto-3g")
 
         mf = scf.RHF(mol)
         mf.conv_tol = 1e-12
@@ -24,6 +24,6 @@ def test_matching_order():
         mybe.optimize(solver="CCSD")
         return mybe.ebe_tot - mybe.ebe_hf
 
-    assert isclose(
+    assert not isclose(
         get_energy("data/octane.xyz"), get_energy("./data/suspected_bug_octane.xyz")
     )
