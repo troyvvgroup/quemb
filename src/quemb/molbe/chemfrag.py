2f1c33fbea6989c5a9687271724494467fe3c3c7
Oskar Weser <oskar.weser@gmail.com>
2025-02-12 10:37:05 -0500

    moved types to typing module, they are interesting in general

--- src/quemb/molbe/chemfrag.py
+++ src/quemb/molbe/chemfrag.py
@@ -22,86 +22,35 @@ There are three main classes:
 from collections import defaultdict
 from collections.abc import Hashable, Mapping, Sequence
 from numbers import Real
-from typing import Callable, Final, NewType, TypeAlias, TypeVar, cast
+from typing import Callable, Final, TypeAlias, TypeVar, cast
 
 import chemcoord as cc
 import numpy as np
 from attr import define
 from chemcoord import Cartesian
 from chemcoord.constants import elements
-from chemcoord.typing import AtomIdx
 from ordered_set import OrderedSet
 from pyscf.gto import Mole
 from typing_extensions import Self, assert_never
 
-from quemb.shared.typing import T
+from quemb.shared.typing import (
+    AOIdx,
+    AtomIdx,
+    CenterIdx,
+    EdgeIdx,
+    FragmentIdx,
+    GlobalAOIdx,
+    MotifIdx,
+    OriginIdx,
+    OtherRelAOIdx,
+    OwnRelAOIdx,
+    T,
+)
 
 # Note that most OrderedSet types could be a non-mutable equivalent here,
 # unfortunately this is not that easy to declare
 # see https://stackoverflow.com/questions/79401030/good-way-to-define-new-protocol-for-collection-types
 
-
-#: The index of an atomic orbital. This is the global index, i.e. not per fragment.
-AOIdx = NewType("AOIdx", int)
-
-#: The global index of an atomic orbital, i.e. not per fragment.
-#: This is basically the result of
-#: the `func:pyscf.gto.mole.Mole.aoslice_by_atom` method.
-GlobalAOIdx = NewType("GlobalAOIdx", AOIdx)
-
-#: The relative AO index.
-#: This is relative to the own fragment.
-OwnRelAOIdx = NewType("OwnRelAOIdx", AOIdx)
-
-#: The relative AO index, relative to another fragment.
-#: For example for an edge in fragment 1 it is the AO index of the same atom
-#: interpreted as center in fragment 2.
-OtherRelAOIdx = NewType("OtherRelAOIdx", AOIdx)
-
-#: The index of a Fragment.
-FragmentIdx = NewType("FragmentIdx", int)
-
-#: The index of a heavy atom, i.e. of a motif.
-#: If hydrogen atoms are not treated differently, then every atom
-#: is a motif, and this type is equivalent to :class:`AtomIdx`.
-MotifIdx = NewType("MotifIdx", AtomIdx)
-#: In a given fragment, this is the index of a center.
-#: A center was used to generate a fragment around it.
-#: Since a fragment can swallow other smaller fragments, there
-#: is only one origin per fragment but multiple centers,
-#: which are the origins of the swallowed fragments.
-CenterIdx = NewType("CenterIdx", MotifIdx)
-#: An edge is the complement of the set of centers in a fragment.
-EdgeIdx = NewType("EdgeIdx", MotifIdx)
-
-#: In a given BE fragment, this is the origin of the remaining
-#: fragment after subsets have been removed.
-#: Since a fragment can swallow other smaller fragments, there
-#: is only one origin per fragment but multiple centers,
-#: which are the origins of the swallowed fragments.
-#:
-#: In the following example, we have drawn two fragments,
-#: one around atom A and one around atom B. The fragment
-#: around atom A is completely contained in the fragment around B,
-#: hence we remove it. The remaining fragment around B has the origin B,
-#: and swallowed the fragment around A.
-#: Hence its centers are A and B.
-#: The set of edges is the complement of the set of centers,
-#: hence in this case for the fragment around B
-#: the set of edges is the one-element set {C}.
-#:
-#: .. code-block::
-#:
-#:    __________
-#:    |        |  BE2 fragment around A
-#:    |        |
-#:    ___________________
-#:    |        |        |   BE2 fragment around B
-#:    |        |        |
-#:    A ------ B ------ C ------ D
-#:    |        |        |        |
-#:
-OriginIdx = NewType("OriginIdx", CenterIdx)
 
 # We would like to have a subtype of Sequence that also behaves generically
 # so that we could write
