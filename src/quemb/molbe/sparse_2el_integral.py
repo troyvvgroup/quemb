4579107dc0c45722ded06b97b82a98a970bb7581
Oskar Weser <oskar.weser@gmail.com>
2025-02-19 16:20:42 -0500

    added sparse 2el integrals

--- src/quemb/molbe/sparse_2el_integral.py
+++ src/quemb/molbe/sparse_2el_integral.py
@@ -1,9 +1,10 @@
 from collections.abc import Hashable, Mapping
 from typing import TypeVar
 
-from numba import typeof, types
+from numba import njit, typeof, types  # type: ignore[attr-defined]
 from numba.experimental import jitclass
 from numba.typed import Dict
+from numba.types import DictType, float64, int64
 
 Key = TypeVar("Key", bound=Hashable)
 Val = TypeVar("Val")
@@ -24,10 +25,18 @@ def to_numba_dict(py_dict: Mapping[Key, Val]) -> Dict[Key, Val]:
     return numba_dict
 
 
-@jitclass
+@njit(cache=True)
+def gauss_sum(n: int) -> int:
+    return (n * (n + 1)) // 2
+
+
+kv_ty = (int64, float64)
+
+
+@jitclass([("_data", DictType(*kv_ty))])
 class TwoElIntegral:
-    """This class is a wrapper around the 2-electron integrals.
-    It is used to store the 2-electron integrals in a sparse format.
+    """Sparsely stores the 2-electron integrals using chemist's notation.
+
     The 2-electron integrals are stored in a dictionary.
     The keys of the dictionary are tuples of the form (i, j, k, l)
     where i, j, k, l are the indices of the basis functions.
@@ -35,10 +44,18 @@ class TwoElIntegral:
     """
 
     def __init__(self) -> None:
-        self._data = Dict.empty(
-            key_type=types.int64,
-            value_type=types.float64,
-        )
+        self._data = Dict.empty(*kv_ty)
+
+    def __getitem__(self, key: tuple[int, int, int, int]) -> types.float64:
+        idx = self.compound(*key)
+        return self._data.get(idx, 0.0)
 
     def __setitem__(self, key: tuple[int, int, int, int], value: float) -> None:
-        self._data[key] = value
+        self._data[self.compound(*key)] = value
+
+    @staticmethod
+    def compound(a: int, b: int, c: int, d: int) -> int:
+        """Return compound index given four indices using Yoshimine sort"""
+        ab = gauss_sum(a) + b if a > b else gauss_sum(b) + a
+        cd = gauss_sum(c) + d if c > d else gauss_sum(d) + c
+        return gauss_sum(ab) + cd if ab > cd else gauss_sum(cd) + ab
