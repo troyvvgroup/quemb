125c21c49241bb465c80d32c648f5059a0114b8d
Oskar Weser <oskar.weser@gmail.com>
2025-04-22 17:05:53 -0500

    finished kbe pfrag renaming

--- src/quemb/kbe/pfrag.py
+++ src/quemb/kbe/pfrag.py
@@ -103,7 +103,7 @@ class Frags:
         self.t2 = None
 
         self.heff = None
-        self.edge = AO_per_edge
+        self.AO_per_edge = AO_per_edge
         self.ref_frag_idx_per_edge = ref_frag_idx_per_edge
         self.relAO_per_edge = relAO_per_edge
         self.relAO_in_ref_per_edge = relAO_in_ref_per_edge
