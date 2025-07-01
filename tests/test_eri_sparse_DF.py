8d63202e8b2e52a8933c1b9c232982bda7cd50da
Oskar Weser <oskar.weser@gmail.com>
2025-06-30 20:58:33 -0400

    fixed test

--- tests/test_eri_sparse_DF.py
+++ tests/test_eri_sparse_DF.py
@@ -99,7 +99,7 @@ def test_sparse_DF_BE() -> None:
 
     assert np.isclose(
         sparse_DF_BE.ebe_tot - sparse_DF_BE.ebe_hf,
-        -0.5498849435531383,
+        -0.5499222005057618,
         atol=1e-10,
         rtol=0,
     )
