7cae3015d4bf16b8e1c11f468fb53141e465bd81
Carina Luo <107739074+cariluo@users.noreply.github.com>
2025-07-13 09:06:49 -0400

    Update config.py
    Tried getting rid of extra spaces when doing basic logging configuration in case that is causing a formatting error

--- src/quemb/shared/config.py
+++ src/quemb/shared/config.py
@@ -28,7 +28,7 @@ from cattrs import structure, unstructure
 from quemb.shared.helper import add_docstring
 
 import logging
-logging.basicConfig( level=logging.DEBUG,   format="%(asctime)s [%(levelname)s] %(name)s: %(message)s" )
+logging.basicConfig(level=logging.DEBUG,format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
 
 DEFAULT_RC_PATH: Final = Path("~/.quembrc.yml")
 
