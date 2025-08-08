9dcf85a79ad7d1cc8cc5b1d94bd30aad2222c9a3
Oskar Weser <oskar.weser@gmail.com>
2025-08-08 12:12:34 -0400

    removed redundant functions

--- src/quemb/molbe/mf_interfaces/__init__.py
+++ src/quemb/molbe/mf_interfaces/__init__.py
@@ -3,8 +3,6 @@ from quemb.molbe.mf_interfaces.main import (
     dump_scf,
     get_mf,
     load_scf,
-    read_hdf5,
-    store_to_hdf5,
 )
 from quemb.molbe.mf_interfaces.orca_interface import OrcaArgs, get_orca_basis
 
@@ -12,8 +10,6 @@ __all__ = [
     "get_mf",
     "dump_scf",
     "load_scf",
-    "read_hdf5",
-    "store_to_hdf5",
     "get_orca_basis",
     "OrcaArgs",
     "AVAILABLE_BACKENDS",
--- src/quemb/molbe/mf_interfaces/main.py
+++ src/quemb/molbe/mf_interfaces/main.py
@@ -3,7 +3,6 @@
 from collections.abc import Mapping
 from typing import Final, Literal
 
-import h5py
 from pyscf.gto import Mole
 from pyscf.lib.chkfile import load, load_mol, save, save_mol
 from pyscf.scf.hf import RHF
@@ -150,40 +149,3 @@ def dump_scf(mf: RHF, chkfile: PathLike) -> None:
         "mo_coeff": mf.mo_coeff,
     }
     save(chkfile, "scf", scf_data)
-
-
-def store_to_hdf5(mf: RHF, path: PathLike) -> None:
-    """Store a PySCF RHF object to an HDF5 file."""
-    with h5py.File(path, "w") as f:
-        mol_group = f.create_group("mol")
-        mol = _force_eval_mol(mf.mol)
-
-        mol_group.attrs["atom"] = mol.atom
-        mol_group.attrs["basis"] = mol.basis
-        mol_group.attrs["unit"] = mol.unit
-        mol_group.attrs["charge"] = mol.charge
-        mol_group.attrs["spin"] = mol.spin
-
-        f.create_dataset("mo_coeff", data=mf.mo_coeff)
-        f.create_dataset("mo_energy", data=mf.mo_energy)
-        f.create_dataset("mo_occ", data=mf.mo_occ)
-        f.attrs["e_tot"] = mf.e_tot
-
-
-def read_hdf5(path: PathLike) -> RHF:
-    """Recreate a PySCF RHF object from an HDF5 file."""
-    with h5py.File(path, "r") as f:
-        mol_data = f["mol"].attrs
-        mol = Mole()
-        mol.atom = mol_data["atom"]
-        mol.basis = mol_data["basis"]
-        mol.unit = mol_data["unit"]
-        mol.charge = int(mol_data["charge"])
-        mol.spin = int(mol_data["spin"])
-        mol.build()
-
-        mo_coeff = f["mo_coeff"][()]
-        mo_energy = f["mo_energy"][()]
-        mo_occ = f["mo_occ"][()]
-        e_tot = float(f.attrs["e_tot"])
-    return create_mf(mol, mo_coeff, mo_energy, mo_occ, e_tot)
