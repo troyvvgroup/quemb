6a438072b770828d4f75aa4090668f77b505d18f
Carina Luo <107739074+cariluo@users.noreply.github.com>
2025-08-14 08:29:55 -0400

    fixed a math mode thing

--- src/quemb/kbe/pfrag.py
+++ src/quemb/kbe/pfrag.py
@@ -68,7 +68,7 @@ class Frags:
             or FragPart.AO_per_frag[i])
             Read more detailed description in :class:`quemb.kbe.fragment.FragPart`.
         ifrag :
-            fragment index (∈ [0, pbe.n_frag - 1])
+            fragment index (:math:`\in [0, pbe.n\_frag - 1]`)
         AO_per_edge :
             list of lists of edge site AOs for each atom in the fragment.
             Read more detailed description in :class:`quemb.kbe.fragment.FragPart`.
