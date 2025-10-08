8f151849fe37bedc04770a0f9052236c40cfca34
Carina Luo <107739074+cariluo@users.noreply.github.com>
2025-10-08 15:03:56 -0400

    fix

--- src/quemb/molbe/pfrag.py
+++ src/quemb/molbe/pfrag.py
@@ -232,8 +232,6 @@ class Frags:
         veff_, veff0 = get_veff(eri_, dm, S, self.TA, hf_veff)
         self.veff = veff_.real
         self.veff0 = veff0
-        self.vj = vj
-        self.vk = vk
         self.fock = self.h1 + veff_.real
 
     def get_nsocc(self, S, C, nocc, ncore=0):
