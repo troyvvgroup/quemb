99b43e5ccc57152de274ea4fb72d40f0e5e44a36
Oskar Weser <oskar.weser@gmail.com>
2025-10-16 05:52:58 -0700

    expose chemgen info (#222)
    Expose additional chemgen info for downstream code.
    
    This allows to specialize via:
    ```python
    if isinstance(x, ChemFragPart):
        x.fragmented....
    ```
    and use the additional available info.

--- src/quemb/molbe/chemfrag.py
+++ src/quemb/molbe/chemfrag.py
@@ -30,7 +30,7 @@ from pathlib import Path
 from typing import Any, Final, Generic, TypeAlias, TypeVar, cast
 
 import numpy as np
-from attrs import cmp_using, define, field
+from attrs import cmp_using, define, field, fields
 from chemcoord import Cartesian
 from networkx.algorithms.shortest_paths.generic import shortest_path_length
 from networkx.classes.graph import Graph
@@ -38,7 +38,7 @@ from ordered_set import OrderedSet
 from pyscf.gto import M, Mole, is_au
 from pyscf.lib import param
 from pyscf.pbc.gto import Cell
-from typing_extensions import Self
+from typing_extensions import Self, override
 
 from quemb.molbe.autofrag import FragPart
 from quemb.molbe.helper import are_equal, get_core
@@ -56,6 +56,7 @@ from quemb.shared.typing import (
     RelAOIdx,
     RelAOIdxInRef,
     T,
+    Vector,
 )
 
 # NOTE: We rely on the fact that post python 3.7 dictionaries preserve insertion order!
@@ -82,6 +83,10 @@ def _iloc(view: Iterable[T], n: int) -> T:
     Scales linearly! Do not use for large n!
     """
     return next(x for i, x in enumerate(iter(view)) if i == n)
+
+
+def _reorder(seq: Sequence[T], idx: Sequence[int] | Vector[np.integer]) -> list[T]:
+    return [seq[i] for i in idx]  # type: ignore[index]
 
 
 def _flatten(nested: Iterable[Iterable[T]]) -> list[T]:
@@ -1041,6 +1046,29 @@ class PurelyStructureFragmented(Generic[_T_chemsystem]):
         """
         return len(self.conn_data.motifs) != sum(len(x) for x in self.centers_per_frag)
 
+    def reorder_frags(self, idx: Sequence[int] | Vector[np.integer]) -> Self:
+        """Reorder the fragments of self.
+
+        Can, for example, be used to order the fragments by size.
+
+        Parameters
+        ----------
+        idx :
+            The new index, has to be a permutation of :python:`[0, ..., len(self)]`.
+        """
+        assert set(idx) == set(range(len(idx)))
+        return self.__class__(
+            mol=self.mol,
+            motifs_per_frag=_reorder(self.motifs_per_frag, idx),
+            centers_per_frag=_reorder(self.centers_per_frag, idx),
+            edges_per_frag=_reorder(self.edges_per_frag, idx),
+            origin_per_frag=_reorder(self.origin_per_frag, idx),
+            atoms_per_frag=_reorder(self.atoms_per_frag, idx),
+            ref_frag_idx_per_edge=_reorder(self.ref_frag_idx_per_edge, idx),
+            conn_data=self.conn_data,
+            n_BE=self.n_BE,
+        )
+
 
 @define(frozen=True, kw_only=True)
 class Fragmented(Generic[_T_chemsystem]):
@@ -1332,8 +1360,8 @@ class Fragmented(Generic[_T_chemsystem]):
         """The number of fragments."""
         return len(self.AO_per_frag)
 
-    def _get_FragPart_no_iao(self) -> FragPart:
-        """Transform into a :class:`quemb.molbe.autofrag.FragPart`
+    def _get_FragPart_no_iao(self) -> ChemFragPart:
+        """Transform into a :class:`quemb.molbe.chemfrag.ChemFragPart`
         for further use in quemb.
 
         Matches the output of :func:`quemb.molbe.autofrag.autogen`.
@@ -1359,7 +1387,7 @@ class Fragmented(Generic[_T_chemsystem]):
         origin_per_frag = list(union_of_seqs(*self.frag_structure.origin_per_frag))
         assert len(origin_per_frag) == len(self)
 
-        return FragPart(
+        return ChemFragPart(
             mol=self.mol,
             frag_type="chemgen",
             n_BE=self.frag_structure.n_BE,
@@ -1392,9 +1420,10 @@ class Fragmented(Generic[_T_chemsystem]):
             frozen_core=self.frozen_core,
             iao_valence_basis=None,
             iao_valence_only=False,
+            fragmented=self,
         )
 
-    def _get_FragPart_with_iao(self, wrong_iao_indexing: bool) -> FragPart:
+    def _get_FragPart_with_iao(self, wrong_iao_indexing: bool) -> ChemFragPart:
         """Transform into a :class:`quemb.molbe.autofrag.FragPart`
         for further use in quemb.
 
@@ -1503,7 +1532,7 @@ class Fragmented(Generic[_T_chemsystem]):
         matched_output_no_iao = self._get_FragPart_no_iao()
 
         # Only are actually different when doing IAOs
-        return FragPart(
+        return ChemFragPart(
             mol=self.mol,
             frag_type="chemgen",
             n_BE=self.frag_structure.n_BE,
@@ -1521,15 +1550,109 @@ class Fragmented(Generic[_T_chemsystem]):
             frozen_core=self.frozen_core,
             iao_valence_basis=self.iao_valence_mol.basis,
             iao_valence_only=False,
+            fragmented=self,
         )
 
-    def get_FragPart(self, wrong_iao_indexing: bool | None = None) -> FragPart:
+    def get_FragPart(self, wrong_iao_indexing: bool | None = None) -> ChemFragPart:
         """Match the output of :func:`quemb.molbe.autofrag.autogen`."""
         if self.iao_valence_mol is None:
             return self._get_FragPart_no_iao()
         else:
             assert wrong_iao_indexing is not None
             return self._get_FragPart_with_iao(wrong_iao_indexing)
+
+    def reorder_frags(self, idx: Sequence[int] | Vector[np.integer]) -> Self:
+        """Reorder the fragments of self.
+
+        Can, for example, be used to order the fragments by size.
+
+        Parameters
+        ----------
+        idx :
+            The new index, has to be a permutation of :python:`[0, ..., len(self)]`.
+        """
+        assert set(idx) == set(range(len(idx)))
+        return self.__class__(
+            mol=self.mol,
+            conn_data=self.conn_data,
+            frag_structure=self.frag_structure.reorder_frags(idx),
+            AO_per_atom=self.AO_per_atom,
+            AO_per_frag=_reorder(self.AO_per_frag, idx),
+            AO_per_motif=self.AO_per_motif,
+            AO_per_edge_per_frag=_reorder(self.AO_per_edge_per_frag, idx),
+            relAO_per_center_per_frag=_reorder(self.relAO_per_center_per_frag, idx),
+            relAO_per_origin_per_frag=_reorder(self.relAO_per_origin_per_frag, idx),
+            relAO_in_ref_per_edge_per_frag=_reorder(
+                self.relAO_in_ref_per_edge_per_frag, idx
+            ),
+            relAO_per_motif_per_frag=_reorder(self.relAO_per_motif_per_frag, idx),
+            relAO_per_edge_per_frag=_reorder(self.relAO_per_edge_per_frag, idx),
+            frozen_core=self.frozen_core,
+            iao_valence_mol=self.iao_valence_mol,
+        )
+
+
+@define(kw_only=True, hash=False)
+class ChemFragPart(FragPart):
+    """Expose chemgen info, but retain old FragPart behaviour for outside code.
+
+    In particular, the equality comparison uses only FragPart info."""
+
+    fragmented: Final[Fragmented] = field(eq=False)
+
+    __hash__ = None  # explicitly mark as unhashable
+
+    def _get_FragPart(self) -> FragPart:
+        return FragPart(
+            **{f.name: getattr(self, f.name) for f in fields(FragPart) if f.init}
+        )
+
+    @override
+    def __eq__(self, other: Any) -> bool:
+        return self._get_FragPart() == other
+
+    @override
+    def __ne__(self, other: Any) -> bool:
+        return not (self == other)
+
+    @override
+    def reorder_frags(self, idx: Sequence[int] | Vector[np.integer]) -> Self:
+        """Reorder the fragments of self.
+
+        Can, for example, be used to order the fragments by size.
+
+        Parameters
+        ----------
+        idx :
+            The new index, has to be a permutation of :python:`[0, ..., len(self)]`.
+        """
+        assert set(idx) == set(range(len(idx)))
+        return self.__class__(
+            mol=self.mol,
+            frag_type=self.frag_type,
+            n_BE=self.n_BE,
+            AO_per_frag=_reorder(self.AO_per_frag, idx),
+            AO_per_edge_per_frag=_reorder(self.AO_per_edge_per_frag, idx),
+            ref_frag_idx_per_edge_per_frag=_reorder(
+                self.ref_frag_idx_per_edge_per_frag, idx
+            ),
+            relAO_per_edge_per_frag=_reorder(self.relAO_per_edge_per_frag, idx),
+            relAO_in_ref_per_edge_per_frag=_reorder(
+                self.relAO_in_ref_per_edge_per_frag, idx
+            ),
+            relAO_per_origin_per_frag=_reorder(self.relAO_per_origin_per_frag, idx),
+            weight_and_relAO_per_center_per_frag=_reorder(
+                self.weight_and_relAO_per_center_per_frag, idx
+            ),
+            motifs_per_frag=_reorder(self.motifs_per_frag, idx),
+            origin_per_frag=_reorder(self.origin_per_frag, idx),
+            H_per_motif=self.H_per_motif,
+            add_center_atom=_reorder(self.add_center_atom, idx),
+            frozen_core=self.frozen_core,
+            iao_valence_basis=self.iao_valence_basis,
+            iao_valence_only=self.iao_valence_only,
+            fragmented=self.fragmented.reorder_frags(idx),
+        )
 
 
 def _get_AOidx_per_atom(
