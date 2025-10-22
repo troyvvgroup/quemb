f71da2f921ee1be7c00e4b110e908c98fa49a6e6
Oskar Weser <oskar.weser@gmail.com>
2025-10-22 17:44:15 -0400

    fixed small error

--- src/quemb/molbe/eri_sparse_DF.py
+++ src/quemb/molbe/eri_sparse_DF.py
@@ -986,7 +986,7 @@ def approx_S_abs(mol: Mole, nroots: int = 500) -> Matrix[np.float64]:
 
 def _ensure_normalization(S_abs: Matrix[np.floating]) -> Matrix[np.float64]:
     N = np.sqrt(np.diag(S_abs))
-    return S_abs * (N[:, None] * N[None, :])
+    return S_abs / (N[:, None] * N[None, :])
 
 
 @timer.timeit
