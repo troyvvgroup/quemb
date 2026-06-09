cfb9db7ed16ba92032c3701fd73e88aa5552ea38
Carina Luo <107739074+cariluo@users.noreply.github.com>
2026-06-09 16:39:46 -0400

    add numerical force embedding test

--- tests/molbe_h6_scanner_test.py
+++ tests/molbe_h6_scanner_test.py
@@ -10,7 +10,14 @@ from pyscf import cc, gto, scf
 from pyscf.tools import finite_diff
 
 from quemb.molbe.chemfrag import ChemGenArgs
-from quemb.molbe.scanner import BEArgs, Energy, be_ref_data, energy_be
+from quemb.molbe.scanner import (
+    BEArgs,
+    Energy,
+    be_ref_data,
+    energy_be,
+    energy_force_emb,
+    force_emb_ref_data,
+)
 
 
 def test_numerical_be_gradient():
@@ -90,6 +97,56 @@ def test_numerical_be_gradient():
     assert np.isclose(E1, -3.23567708251885)
     assert np.isclose(
         np.sqrt(np.mean((ref_grad_corr1 - fd_grad1) ** 2)), 5.8664061568865036e-08
+    )
+
+
+def test_numerical_force_emb_gradient():
+    mol0 = gto.M(
+        atom="""
+            H  0.04 -0.02  0.
+            H  0.02  0.04  1.
+            H -0.01 -0.00  2.
+            H -0.03  0.01  3.
+            H -0.00  0.04  4.
+            H  0.04  0.02  5.
+            """,
+        basis="sto-3g",
+        charge=0,
+    )
+
+    # CCSD analytic gradient of mol0
+    mf = scf.RHF(mol0)
+    mf.conv_tol = 1e-12
+    mf.kernel()
+    mc = cc.CCSD(mf)
+    mc.kernel()
+    mc_grad = mc.nuc_grad_method()
+    ref_grad_corr = mc_grad.kernel()
+
+    # build force embedding energy method
+    be_args = BEArgs(
+        n_BE=3,
+        solver="CCSD",
+        use_cumulant=True,
+        optimize=False,
+        additional_args=ChemGenArgs(h_treatment="treat_H_like_heavy_atom"),
+    )
+    energy_method = Energy(
+        mol0, energy_force_emb, energy_args=be_args, ref_data_func=force_emb_ref_data
+    )
+
+    # BE3-CCSD force embedding numerical gradient
+    fd_grad = finite_diff.kernel(energy_method, displacement=1e-4)
+    assert np.isclose(
+        np.sqrt(np.mean((ref_grad_corr - fd_grad) ** 2)), 5.904036799315e-08
+    )
+
+    # BE3-CCSD oneshot numerical gradient using pyscf built-in Gradients object
+    grad_method = finite_diff.Gradients(energy_method)
+    grad_method.displacement = 1e-4
+    fd_grad_fromObj = grad_method.kernel()
+    assert np.isclose(
+        np.sqrt(np.mean((ref_grad_corr - fd_grad_fromObj) ** 2)), 5.904036799315e-08
     )
 
 
@@ -176,4 +233,5 @@ def test_numerical_be_gradient_withmatching():
 
 if __name__ == "__main__":
     test_numerical_be_gradient()
+    test_numerical_force_emb_gradient()
     test_numerical_be_gradient_withmatching()
