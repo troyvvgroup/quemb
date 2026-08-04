be8329f4ffe17ccea881e05c5a0a541d572ce066
Carina Luo <107739074+cariluo@users.noreply.github.com>
2026-08-04 13:48:33 -0400

    remove obselete tests and helper functions

--- tests/molbe_io_fcidump_test.py
+++ tests/molbe_io_fcidump_test.py
@@ -1,14 +1,12 @@
 # Illustrates how fcidump file containing fragment hamiltonian
 # can be generated using be2fcidump
 
-import os
 from pathlib import Path
 from shutil import rmtree
 from tempfile import mkdtemp
 
 import h5py
 import numpy as np
-import pytest
 from numpy import einsum
 from pyscf import ao2mo
 from pyscf.lib.misc import with_omp_threads
@@ -37,26 +35,6 @@ def prepare_system() -> BE:
     fobj = fragmentate(n_BE=2, mol=mol)
     oct_be = BE(mf, fobj)
     return oct_be
-
-
-def verify_fcidump_writing(kind_of_MO: str) -> None:
-    oct_be = prepare_system()
-    tmp_dir = Path(mkdtemp())
-    data_dir = Path("data/octane_FCIDUMPs/")
-    (tmp_dir / kind_of_MO).mkdir()
-
-    # Write out fcidump file for each fragment
-    be2fcidump(oct_be, str(tmp_dir / kind_of_MO / "octane"), kind_of_MO)
-
-    for i in range(6):
-        reference = fcidump.read(data_dir / kind_of_MO / f"octanef{i}")
-        new = fcidump.read(tmp_dir / kind_of_MO / f"octanef{i}")
-
-        for key in ["H1", "H2"]:
-            if not np.allclose(new[key], reference[key]):
-                print(abs(new[key] - reference[key]).max())
-                raise ValueError("too large difference")
-    rmtree(tmp_dir)
 
 
 def verify_fcidump_io(kind_of_MO: str) -> None:
@@ -114,22 +92,6 @@ def verify_fcidump_io(kind_of_MO: str) -> None:
     rmtree(tmp_dir)
 
 
-@pytest.mark.skipif(
-    not os.getenv("QUEMB_DO_KNOWN_TO_FAIL_TESTS") == "true",
-    reason="This test is known to fail.",
-)
-def test_embedding() -> None:
-    verify_fcidump_writing("embedding")
-
-
-@pytest.mark.skipif(
-    not os.getenv("QUEMB_DO_KNOWN_TO_FAIL_TESTS") == "true",
-    reason="This test is known to fail.",
-)
-def test_fragment_mo() -> None:
-    verify_fcidump_writing("fragment_mo")
-
-
 def test_fcidump_io_embedding() -> None:
     verify_fcidump_io("embedding")
 
@@ -139,7 +101,5 @@ def test_fcidump_io_fragment_mo() -> None:
 
 
 if __name__ == "__main__":
-    test_embedding()
-    test_fragment_mo()
     test_fcidump_io_embedding()
     test_fcidump_io_fragment_mo()
